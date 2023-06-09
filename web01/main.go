e9f3226ded393ee9bb6231a082af18b12024cb40
jmjf <jamee.mikell@gmail.com>
2023-06-09 18:59:10 +0000

    FEAT: accept a query parameter; add separate routes for JSON and HTML text

--- web01/main.go
+++ web01/main.go
@@ -3,6 +3,7 @@ package main
 // [net/http](https://pkg.go.dev/net/http) is the golang http package
 // it provides both http call methods and http server methods
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
@@ -10,10 +11,44 @@ import (
 func main() {
 	// HandleFunc registers a handler function for a route
 	// Handlers accept a ResponseWriter and a *Request
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
+		// Query parameters are in the request
+		// Request includes a lot of useful information including Method, URL, protocol information, headers, etc.
+		// The URL is an object that includes query parameters (RawQuery)
+		// Query() returns RawQuery as a map[string][string]
+		queryParams := request.URL.Query()
+		// let's show the URI and query parameters so we know what we're getting
+		log.Println("INFO Received", request.RequestURI, queryParams)
+
+		name := queryParams.Get("name")
+		// alternate styles
+		// name := request.URL.Query().Get("name")
+		// name := queryParams["name"]
+		// name := request.URL.Query()["name"][0] // could have more than one with this style
+
 		// Write writes data to the HTTP connection.
 		// It accepts a slice of bytes, so we need to cast here.
-		w.Write([]byte("Hello, world."))
+		response.Write([]byte("<h1>Hello, " + name + ".</h1>"))
+	})
+
+	// Let's do a JSON result
+	http.HandleFunc("/json", func(response http.ResponseWriter, request *http.Request) {
+		queryParams := request.URL.Query()
+
+		log.Println("INFO Received", request.RequestURI, queryParams)
+
+		name := queryParams.Get("name")
+
+		// To return JSON, we need to build something we can convert to a JSON string
+		jsonMap := map[string]string{
+			"message": "Hello",
+			"name":    name,
+		}
+		// And we need a JSON encoder
+		// The JSON encoder accepts a writer. Anything we encode to it is written to the writer.
+		jsonEncoder := json.NewEncoder(response)
+		// Now we can encode the JSON map as a string
+		jsonEncoder.Encode(jsonMap)
 	})
 
 	// ListenAndServe starts listening for HTTP requests.
